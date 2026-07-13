2cc759e1c521cba9dd358a2b5892c20c50294c5e
Lorenzo Marci <marci.lorenzo@gmail.com>
2026-07-13 13:48:47 +0200

    chore(engine): scaffold pure-JVM engine-core module

---
+++ engine-core/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "2.1.0"
+    kotlin("plugin.serialization") version "2.1.0"
+}
+repositories { mavenCentral() }
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
+}
+tasks.test { useJUnitPlatform() }
+kotlin { jvmToolchain(17) }
